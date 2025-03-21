d4adc4f71f4ebe297e6b681a524f405936bfa382
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-03-21 16:42:21 +0800

    Update and rename ip_del.py to alive_ip.py

--- alive_ip.py
+++ alive_ip.py
@@ -1,16 +1,18 @@
 import re
 
+PATH = './ip/'
+    
 if "__name__==__main__":#主程序开始
 
-    with open('./ip/ALLIP.txt', 'r', encoding='utf-8') as f:
+    with open(f'{PATH}ALLIP.txt', 'r', encoding='utf-8') as f:
         ip_all = f.read()
     ip_list = re.split(r'\n+',ip_all)
 
-    with open('ip_node.txt', 'r', encoding='utf-8') as f:
+    with open(f'{PATH}node.txt', 'r', encoding='utf-8') as f:
         ip_node_all = f.read()
     ip_node_list = re.split(r'\n+',ip_node_all)
     
-    new_ip_list = []
+    alive_ip = []
     for ip in ip_list:
         if ip == '':
             continue
@@ -20,11 +22,11 @@ if "__name__==__main__":#主程序开始
             ipaddr = ip
         for node in ip_node_list:
             if ipaddr in node:
-                new_ip_list.append(ip)
+                alive_ip.append(ip)
                 print(ip)
                 continue
     #去重后保存
-    ip_del = '\n'.join(new_ip_list)
-    with open(f'./ip/ip_node.txt', 'w', encoding='utf-8') as f:
-        f.write(ip_del)
+    alive_ip = '\n'.join(alive_ip)
+    with open(f'{PATH}/checked/node_ip.txt', 'w', encoding='utf-8') as f:
+        f.write(alive_ip)
     
