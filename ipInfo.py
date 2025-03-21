b3275003f52df1b2cb7c312d41118b2e64f96b06
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-03-21 11:38:55 +0800

    Update ipInfo.py

--- ipInfo.py
+++ ipInfo.py
@@ -17,23 +17,47 @@ def get_address(ip):
         pass
         return '未知'
 
-
+def list_rm(urlList):#列表去重
+    begin = 0
+    rm = 0
+    length = len(urlList)
+    print(f'\n-----去重开始-----\n')
+    while begin < length:
+        proxy_compared = urlList[begin]
+        begin_2 = begin + 1
+        while begin_2 <= (length - 1):
+            if proxy_compared == urlList[begin_2]:
+                urlList.pop(begin_2)
+                length -= 1
+                begin_2 -= 1
+                rm += 1
+            begin_2 += 1
+        begin += 1
+    print(f'重复数量 {rm}\n-----去重结束-----\n')
+    print(f'剩余总数 {str(len(urlList))}\n')
+    return urlList
+    
 if "__name__==__main__":#主程序开始
 
     with open('ip.txt', 'r', encoding='utf-8') as f:
         ip_all = f.read()
     ip_list = re.split(r'\n+',ip_all)
+    #去重
+    ip_list = list_rm(ip_list)
+    
+    #去重后保存
+    #ip_rm = '\n'.join(ip_list)
+    #with open(f'./ip/ip_rm.txt', 'w', encoding='utf-8') as f:
+        #f.write(ip_rm)
+    
     
     ALLIP = []
     HKIP = []
+    TWIP = []
+    JPIP = []
     SGIP = []
-    CAIP = []
-    USIP = []
     KRIP = []
-    RUIP = []
-    GBIP = []
-    NLIP = []
-    DEIP = []
+    USIP = []
     otherIP= []
     for i in range(len(ip_list)):
         try:
@@ -48,45 +72,36 @@ if "__name__==__main__":#主程序开始
             
             if country_info == 'HK':
                 HKIP.append(ip_list[i])
+            elif country_info == 'TW':
+                TWIP.append(ip_list[i])
+            elif country_info == 'JP':
+                JPIP.append(ip_list[i])
             elif country_info == 'SG':
                 SGIP.append(ip_list[i])
-            elif country_info == 'CA':
-                CAIP.append(ip_list[i])
-            elif country_info == 'US':
-                USIP.append(ip_list[i])
             elif country_info == 'KR':
                 KRIP.append(ip_list[i])
-            elif country_info == 'RU':
-                RUIP.append(ip_list[i])
-            elif country_info == 'GB':
-                GBIP.append(ip_list[i])
-            elif country_info == 'NL':
-                NLIP.append(ip_list[i])
-            elif country_info == 'DE':
-                DEIP.append(ip_list[i])
+            elif country_info == 'US':
+                USIP.append(ip_list[i])
             else:
                 ip_list[i] = ip_list[i] + '_other'
                 otherIP.append(ip_list[i])
-            ALLIP.append(ip_list[i])#需要按国家排序的话,#号掉这个开启下面的"""
+            #ALLIP.append(ip_list[i])#需要按国家排序的话,#号掉这个开启下面的"""
             print(ip_list[i])
         except Exception as e:#万能异常
             print(f'{ip_list[i]}出现错误,错误内容如下：\n{e}')
             pass
 
-    """
-    #按国家顺序排列
+    #"""
+    #按地区顺序排列
     ALLIP = []
     ALLIP.extend(HKIP)
+    ALLIP.extend(TWIP)
+    ALLIP.extend(JPIP)
     ALLIP.extend(SGIP)
-    ALLIP.extend(CAIP)
-    ALLIP.extend(USIP)
-    ALLIP.extend(RUIP)
     ALLIP.extend(KRIP)
-    ALLIP.extend(GBIP)
-    ALLIP.extend(NLIP)
-    ALLIP.extend(DEIP)
+    ALLIP.extend(USIP)
     ALLIP.extend(otherIP)
-    """
+    #"""
     
     ALLIP = '\n'.join(ALLIP)
     with open(f'./ip/ALLIP.txt', 'w', encoding='utf-8') as f:
@@ -95,12 +110,19 @@ if "__name__==__main__":#主程序开始
     HKIP = '\n'.join(HKIP)
     with open(f'./ip/HKIP.txt', 'w', encoding='utf-8') as f:
         f.write(HKIP)
+    TWIP = '\n'.join(TWIP)
+    with open(f'./ip/TWIP.txt', 'w', encoding='utf-8') as f:
+        f.write(TWIP)
+    JPIP = '\n'.join(JPIP)
+    with open(f'./ip/JPIP.txt', 'w', encoding='utf-8') as f:
+        f.write(JPIP)
     SGIP = '\n'.join(SGIP)
     with open(f'./ip/SGIP.txt', 'w', encoding='utf-8') as f:
         f.write(SGIP)
-    USIP = '\n'.join(USIP)
-    with open(f'./ip/USIP.txt', 'w', encoding='utf-8') as f:
-        f.write(USIP)
     KRIP = '\n'.join(KRIP)
     with open(f'./ip/KRIP.txt', 'w', encoding='utf-8') as f:
         f.write(KRIP)
+    USIP = '\n'.join(USIP)
+    with open(f'./ip/USIP.txt', 'w', encoding='utf-8') as f:
+        f.write(USIP)
+
