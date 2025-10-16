0491cdf948334af072af6d45bed401fc69057e32
rxsweet <105535100+rxsweet@users.noreply.github.com>
2025-10-16 17:33:31 +0800

    Update ip_collect.py

--- py/ip_collect.py
+++ py/ip_collect.py
@@ -4,6 +4,7 @@ import json
 
 #保存路径
 SAVE_PATH = './ip/'
+Exball_SAVE_PATH = './ip/exball/'
 #https://raw.githubusercontent.com/6Kmfi6HP/proxy_files/main/HK.txt
 """
 apiList={
@@ -34,6 +35,89 @@ apiList={
   'kr':'https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sub/country_proxies/KR.txt',
   'us':'https://raw.githubusercontent.com/NiREvil/vless/refs/heads/main/sub/country_proxies/US.txt',
   }
+exball_apiList={
+  'hk':'https://ghraw.eu.org/exball/sing-box-config/refs/heads/Master/proxy-scaner/proxy_list/proxyList_HK.txt',
+  'tw':'https://ghraw.eu.org/exball/sing-box-config/refs/heads/Master/proxy-scaner/proxy_list/proxyList_TW.txt',
+  'jp':'https://ghraw.eu.org/exball/sing-box-config/refs/heads/Master/proxy-scaner/proxy_list/proxyList_JP.txt',
+  'sg':'https://ghraw.eu.org/exball/sing-box-config/refs/heads/Master/proxy-scaner/proxy_list/proxyList_SG.txt',
+  'kr':'https://ghraw.eu.org/exball/sing-box-config/refs/heads/Master/proxy-scaner/proxy_list/proxyList_KR.txt',
+  'us':'https://ghraw.eu.org/exball/sing-box-config/refs/heads/Master/proxy-scaner/proxy_list/proxyList_US.txt',
+  }
+
+class Exball:
+    def saveIP(configList):#整理保存
+        allip = []
+        port443 = []
+        for key,value in configList.items():
+            #保存区域
+            try:
+                #添加区域标注
+                new_list = []
+                ip_list = re.split(r'\n+',value)
+                for ip in ip_list:
+                    ipinfo = ip.split(',')
+                    ipaddr = ipinfo[0]
+                    port   = ipinfo[1]
+                    country= ipinfo[2]
+                    new_list.append(ipaddr+':'+port+'#'+country+'_'+'Exball')
+                
+                new_ip_list = '\n'.join(new_list)
+                print(new_ip_list)
+                with open(f'{SAVE_PATH}{key}.txt', 'w', encoding='utf-8') as f:
+                    f.write(new_ip_list)
+                print(f'save {key}.txt 完成！')
+                allip.append(new_ip_list)
+            except requests.exceptions.RequestException as e:  
+                #print(e)
+                print(F'获取{key}写入错误!')
+                pass
+        
+        #将IP合并保存
+        allip = ''.join(allip)
+        with open(f'{SAVE_PATH}allip.txt', 'w', encoding='utf-8') as f:
+            f.write(allip)
+        print(f'save allip.txt 完成！')
+        
+        allip = re.split(r'\n+',allip)
+        #筛选443端口IP
+        for ip in allip:
+            if ':443' in ip and '#us' not in ip:
+                #print(ip + 'haha' + '\n')
+                ip = ip.split(":")[0]
+                port443.append(ip)
+        #保存443端口IP
+        port443 = '\n'.join(port443)
+        with open(f'{SAVE_PATH}port443.txt', 'w', encoding='utf-8') as f:
+            f.write(port443)
+        print(f'save port443.txt 完成！')
+        
+        
+    def getContent(url):#获取网站的内容，将获取的内容返回
+        headers={
+        "User-Agent":"okhttp/3.15",
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        }
+        try:
+            r=requests.get(url,headers=headers, timeout=5.0)
+            if r.status_code==200:
+                r.encoding='utf-8'    #编码方式
+                print(f'{url}网站收集IP完成。')
+                #print(r.text)
+                return r.text
+        except requests.exceptions.RequestException as e:  
+            #print(e)
+            print(F'getContent()功能中出现的错误！获取{url}内容失败，或者打开网址错误!')
+            pass
+
+    def fetchIP(apiList):#获取列表网站的ip内容
+        ipList={}
+        for key,value in apiList.items():
+            config=Exball.getContent(value)
+            if config:
+                ipList[key]=config
+        return ipList
+
+
 def saveIP(configList):#整理保存
     allip = []
     port443 = []
@@ -102,4 +186,8 @@ if "__name__==__main__":#主程序开始
     configList = fetchIP(apiList)
     #整理保存
     saveIP(configList)
+    #收集IP
+    ExballconfigList = Exball.fetchIP(exball_apiList)
+    #整理保存
+    Exball.saveIP(ExballconfigList)
     
