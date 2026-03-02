872cc613574c30601e7e2b967e6feef56a53e4fb
rxsweet <105535100+rxsweet@users.noreply.github.com>
2026-03-02 15:38:08 +0800

    Update ip_alive.py

--- py/ip_alive.py
+++ py/ip_alive.py
@@ -1,11 +1,35 @@
 import re
+import requests
 
 PATH = './ip/'
-    
-if "__name__==__main__":#主程序开始
 
-    with open(f'{PATH}allip.txt', 'r', encoding='utf-8') as f:
-        ip_all = f.read()
+
+def getUrlContent(url):  
+    print(f'获取all_ip:{url}')
+    headers={
+        "User-Agent":"okhttp/3.15",
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
+        }
+    try:
+        r=requests.get(url,headers=headers, timeout=3.0)
+        #print(r.text)
+        if r.status_code==200:
+            r.encoding='utf-8'    #编码方式
+            #print(r.text)
+            print('获取成功！')
+            return r.text
+    except requests.exceptions.RequestException as e:  
+        print(e)
+        print('getUrlContent()功能中出现的错误！获取ip失败，或者打开网址错误!')
+        return ''
+
+if "__name__==__main__":#主程序开始
+    
+    #使用在线获取all ip，不用自己手动复制了
+    ip_all = getUrlContent('https://ghraw.eu.org/rxsweet/cfip/main/ip/allip.txt')
+    #关闭了下面的读取allip文件，使用了上面的，需要使用时打开
+    #with open(f'{PATH}allip.txt', 'r', encoding='utf-8') as f:
+        #ip_all = f.read()
     ip_list = re.split(r'\n+',ip_all)
 
     with open(f'{PATH}node.txt', 'r', encoding='utf-8') as f:
@@ -27,7 +51,7 @@ if "__name__==__main__":#主程序开始
                 continue
     #保存
     alive_ip_str = '\n'.join(alive_ip)
-    with open(f'{PATH}/checked/all.txt', 'w', encoding='utf-8') as f:
+    with open(f'{PATH}aliveip.txt', 'w', encoding='utf-8') as f:
         f.write(alive_ip_str)
 
     #暂时不需要分区了
