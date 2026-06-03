6478698f2fc637057e0f903184a870c67129125c
rxsweet <105535100+rxsweet@users.noreply.github.com>
2026-06-03 16:18:37 +0800

    Update ip_collect_new.py

--- py/ip_collect_new.py
+++ py/ip_collect_new.py
@@ -17,7 +17,6 @@ URLS = [
     "https://raw.githubusercontent.com/mdsdtech/chonky-orange-cat/refs/heads/main/Orange/alivecat.txt",
     "https://raw.githubusercontent.com/afrcloud07/ListProxy/refs/heads/main/proxyip.txt",
     "https://raw.githubusercontent.com/papapapapdelesia/Emilia/refs/heads/main/Data/alive.txt",
-    "https://raw.githubusercontent.com/SherlyKinan/proxy-check/refs/heads/main/dead.txt",
     "https://raw.githubusercontent.com/SherlyKinan/proxy-check/refs/heads/main/active.txt",
     "https://raw.githubusercontent.com/rxsweet/scan-proxyip/refs/heads/Master/proxy-scaner/active-proxy-history.txt",
 ]
